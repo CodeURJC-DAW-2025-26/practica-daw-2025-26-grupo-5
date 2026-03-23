9ed4bf8c3cb14eafcae594dfdf2e4bb7c8b24de0
CodVictor <vhop2004@gmail.com>
2026-03-23 10:20:12 +0100

    docs: formating api-docs.yml

--- backend/src/main/java/es/stilnovo/library/model/User.java
+++ backend/src/main/java/es/stilnovo/library/model/User.java
@@ -13,7 +13,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 
 /**
